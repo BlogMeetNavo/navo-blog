bf65765da9f5dfa7374535c7686ecebe31d8b9a8
BlogMeetNavo <rehuman@greatscott.com.au>
2026-05-25 14:06:01 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,13 @@
 module.exports = function(eleventyConfig) {
-  // Copy static assets
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("public");
 
-  // Tell Eleventy to also watch the content folder
-  eleventyConfig.addWatchTarget("content/");
-
-  // Add content/posts as a collection
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("content/posts/**/*.md").reverse();
-  });
-
-  // Date filter
   eleventyConfig.addFilter("dateDisplay", function(date) {
     return new Date(date).toLocaleDateString("en-AU", {
       year: "numeric", month: "long", day: "numeric"
     });
   });
 
-  // Read time filter
   eleventyConfig.addFilter("readTime", function(content) {
     const words = content.split(" ").length;
     const minutes = Math.ceil(words / 200);
